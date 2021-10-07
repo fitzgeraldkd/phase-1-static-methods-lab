9069f52f7a95e652efaab1b137ef85eed156885d
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-10-07 12:19:38 -0400

    commit tree 1ab939c09d772d3d334e400eec638642dce2be5c

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replaceAll(/[^A-Z0-9 '-]/gi, '');
+  }
+
+  static titleize(str) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return str.split(' ').map((word, index) => {
+      if (exceptions.includes(word) && index !== 0) {
+        return word;
+      } else {
+        return Formatter.capitalize(word);
+      }
+    }).join(' ');
+  }
 }
